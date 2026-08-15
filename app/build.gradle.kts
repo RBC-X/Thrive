70d48db9dedfe17fc6fad8de104ca56d12741e78
bsmit <bsmit@example.com>
2026-08-15 18:27:32 -0400

    Thrive 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.thrive.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "1.4.0"
+        versionCode = 32
+        versionName = "1.4.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
