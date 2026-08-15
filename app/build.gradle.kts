b309c85fc718da8f056b069e5e7dc75f755a28a9
bsmit <bsmit@example.com>
2026-08-14 20:39:20 -0400

    Thrive 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "com.thrive.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
+        versionCode = 22
         versionName = "1.3.2"
     }
 
